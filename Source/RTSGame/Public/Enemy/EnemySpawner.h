17e4e4c74a38eed6defc0be623c258eff19b98b9
Fikunner <93440314+Fikunner@users.noreply.github.com>
2023-04-24 18:26:17 +0200

    Lose screen

---
+++ Source/RTSGame/Public/Enemy/EnemySpawner.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/BoxComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemySpawner.generated.h"
+
+UCLASS()
+class RTSGAME_API AEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemySpawner();
+
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* BoxComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
