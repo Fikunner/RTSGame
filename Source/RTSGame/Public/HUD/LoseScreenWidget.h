5d36d50cd9e13e4b2646d10a93c86d34b7a7bdf1
Bartosz Nowak <93440314+Fikunner@users.noreply.github.com>
2023-04-28 18:26:40 +0200

    Check

---
+++ Source/RTSGame/Public/HUD/LoseScreenWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LoseScreenWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTSGAME_API ULoseScreenWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
